4cdf5c63dbf5d6da2b2d16095d6a28b90a0bafea
Kyra Smith <kyrasmith@Kyras-Air.attlocal.net>
2025-01-06 20:29:05 -0600

    initial

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoList from './components/TodoList/TodoList';
+
+function App() {
+  return (
+   <>
+    <h1>This is App component</h1>
+    <TodoList/>
+    
+   </>
+
+   
+  );
+}
+
+export default App;
